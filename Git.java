0e3b7fd2257d09475918dc5d797027e725b22f52
Max Duque <duquem713@gmail.com>
2019-08-21 12:28:17 -0600

    Joeeeee you dog

--- Git.java
+++ Git.java
@@ -3,6 +3,7 @@ public class Git
 	public static void main (String[] args)
 	{
 		System.out.println("\"git is lit ahahah\"");
-		System.out.prinln("Hello mother.");
+		System.out.println("Hello mother.");
+		System.out.println("Hey Joe!");
 	}
 }
